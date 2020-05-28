4e33bd252c8925100e2f70094a10f9b347b61dcc
Berkley Brown <berkleydeane@gmail.com>
2020-05-28 16:10:12 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,12 @@
 require 'pry'
 
 def my_find(collection)
-
+  index = 0
+  
+  while index < collection.length
+    if yield(collection[index])
+      return collection[index]
+   end
+     index += 1
+ end
 end
